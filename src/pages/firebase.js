9719f000520b8b6f78bb883acf6ee388c9dd6f88
P Sai Charan <papinenisaicharan@gmail.com>
2023-04-21 18:38:57 +0530

    update

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAPLifTFL33dvsdAcLhZWSqtvbR72qHop8",
-  authDomain: "auth-7ec71.firebaseapp.com",
-  databaseURL: "https://auth-7ec71-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "auth-7ec71",
-  storageBucket: "auth-7ec71.appspot.com",
-  messagingSenderId: "999701155447",
-  appId: "1:999701155447:web:51ea5cbadff74c0acf8072"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
@@ -22,4 +19,3 @@ const user = auth.currentUser;
 const googleAuth = new GoogleAuthProvider();
 
 export { app, auth, db, googleAuth };
-
